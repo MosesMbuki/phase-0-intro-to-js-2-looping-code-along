3ac8056c167e84d81f3a36a4fc4f761b70d1410a
Moses Mbuki Mutitu <moses.mutitu@student.moringaschool.com>
2025-03-02 18:21:26 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const messages = writeCards(names, event);
+
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+const startingNumber = 10 
+
+function countDown(startingNumber){
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
+
+
+console.log(messages);
